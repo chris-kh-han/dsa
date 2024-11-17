75ca022fac2b419c5c2b4e061a297345cabcfda6
Chris Han <chris.kh.han@gmail.com>
2024-11-17 02:24:54 -0600

    739 lc

---
+++ 739_daily_temperature.py
@@ -0,0 +1,13 @@
+def dailyTemperatures(temperatures: list[int]) -> list[int]:
+    res = [0] * len(temperatures)
+    stack = []  # pair: [temp, index]
+
+    for i, t in enumerate(temperatures):
+        while stack and t > stack[-1][0]:
+            stackT, stackInd = stack.pop()
+            res[stackInd] = i - stackInd
+        stack.append((t, i))
+    return res
+
+
+print(dailyTemperatures([30, 38, 30, 36, 35, 40, 28]))
